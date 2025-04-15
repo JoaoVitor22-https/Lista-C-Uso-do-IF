14c876c6b714a793102e1b9598bfb6048d0ee528
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-15 10:19:00 -0300

    5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 3 == 0)
+        {
+            Console.WriteLine($"{numero} é múltiplo de 3.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} não é múltiplo de 3.");
+        }
+    }
+}
